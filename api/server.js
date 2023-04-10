f5fcd2faefd8627633a5038c5a6366ae7632dc9d
EmreSahiner <emre@journeyapp.com>
2023-04-10 11:53:08 +0300

    routerlar eklendi

--- api/server.js
+++ api/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const server = express();
 
+const userRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+const orderRouter = require('./orders/orders-router');
+const malzemeRouter = require('./malzemeler/malzemeler-router');
+const pizzaRouter = require('./pizzas/pizzas-router');
+
+server.use('/api/auth', authRouter);
+server.use('/api/user', userRouter);
+server.use('/api/order', orderRouter);
+server.use('/api/malzeme', malzemeRouter);
+server.use('/api/pizza', pizzaRouter);
 
 module.exports = server;
