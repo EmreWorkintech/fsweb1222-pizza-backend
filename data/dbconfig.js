0284f12d3afc3df7d0fb2e684019e3276ea930a6
EmreSahiner <emre@journeyapp.com>
2023-04-10 11:13:34 +0300

    server ve veritabanı setup'ları yapıldı

---
+++ data/dbconfig.js
@@ -0,0 +1,6 @@
+const knex = require('knex');
+const {NODE_ENV} = require('../config/config');
+const config = require('../knexfile');
+
+module.exports = knex(config[NODE_ENV]);
+
---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require('./api/server');
+const {PORT} = require('./config/config');
+
+
+server.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
